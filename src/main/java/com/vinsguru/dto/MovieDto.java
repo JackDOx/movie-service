badc1cc51721ca3e4748012493be647f2aae1eb3
qduydo <jackydo1974@gmail.com>
2025-08-01 15:52:54 -0700

    Add comment to MovieDto

--- src/main/java/com/vinsguru/dto/MovieDto.java
+++ src/main/java/com/vinsguru/dto/MovieDto.java
@@ -2,6 +2,7 @@ package com.vinsguru.dto;
 
 import com.vinsguru.domain.Genre;
 
+// Movie DTO
 public record MovieDto(Integer id,
                        String title,
                        Integer releaseYear,
